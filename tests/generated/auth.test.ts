1303ac7865c02253a771c059154ed39e112db8a2
Yuchan Lee <lasuillard@gmail.com>
2026-08-26 13:07:12 +0000

    Remove stale TODO comments

--- tests/generated/auth.test.ts
+++ tests/generated/auth.test.ts
@@ -1,5 +1,4 @@
 import { it } from "^/tests/_helpers/vitest";
 
-// TODO: Not tested, because it requires OAuth flow (may require web browser)
-it.skip("authorize");
-it.skip("getOrRefreshToken");
+it.todo("authorize");
+it.todo("getOrRefreshToken");
