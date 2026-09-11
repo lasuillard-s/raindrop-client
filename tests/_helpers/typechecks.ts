eb4590b1fe3c10a21ad79a9e9ad099f9163d00ec
Yuchan Lee <lasuillard@gmail.com>
2026-09-11 00:49:07 +0000

    Address Vitest API changes

--- tests/_helpers/typechecks.ts
+++ tests/_helpers/typechecks.ts
@@ -1,9 +1,11 @@
 import { exec } from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
-import type { Task, Use } from "@vitest/runner";
+import type { RunnerTask } from "vitest";
 import { expect } from "vitest";
 import { taskId } from "./common";
+
+type Use<T> = (resource: T) => Promise<void>;
 
 export interface CreateTest {
 	// Test identifier
@@ -29,7 +31,7 @@ export interface RegisterHookArgs {
 export type RegisterHook = (args: RegisterHookArgs) => void;
 
 export async function generateTypeTest(
-	{ task }: { task: Task },
+	{ task }: { task: RunnerTask },
 	use: Use<RegisterHook>,
 ) {
 	const hookFn: RegisterHook = (args: RegisterHookArgs) => {
